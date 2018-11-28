2fab4689f022b000f6a4b48d1fdd2ba21ec3d034
Huw Christianson <hc1413@york.ac.uk>
2018-11-28 19:32:23 +0000

    Initial commit
    Please Note I have pushed everything needed for a working build of a
    test file. I am to tired to figure out which bits of this should be
    ignored. feel free to do some cleaning. will be new pushes on a sub
    branch though so be sure to tell me what needs to go.

---
+++ desktop/src/com/craig/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.craig.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.craig.game.CraigGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CraigGame(), config);
+	}
+}
